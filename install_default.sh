3f0846c0d87cf1587aec82fe2a8e30c431756bda
HyperionU <108021531+HyperionU@users.noreply.github.com>
2024-04-11 22:01:06 +0000

    add bash scripts

---
+++ install_default.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Default ex.
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension bierner.github-markdown-preview
+
+# Inc. in slim
+
+code --install-extension htmlhint.vscode-htmlhint
+
+# Inc. in superslim
+
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension marp-team.marp-vscode
+code --install-extension GitHub.vscode-github-actions
---
+++ install_slim.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Inc. in superslim
+
+code --install-extension marp-team.marp-vscode
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension GitHub.vscode-github-actions
